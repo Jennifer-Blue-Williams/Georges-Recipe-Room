d976c821a5ab53377cfaeb57ec97421a2d2402cb
Jennifer Williams <jennifer.blue.williams@gmail.com>
2022-06-16 10:14:25 -0500

    Set up back end method for GetAllRecipes

---
+++ GeorgesRecipeRoomFullStack/Controllers/RecipeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using GeorgesRecipeRoomFullStack.Repositories;
+using GeorgesRecipeRoomFullStack.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+namespace GeorgesRecipeRoomFullStack.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly IRecipeRepository _recipeRepo;
+        //private readonly ITagRepository _tagRepo;
+        private readonly IUserProfileRepository _userRepo;
+
+        public RecipeController(IRecipeRepository recipeRepository, IUserProfileRepository userProfileRepository)
+          //Add above ITagRepository tagRepository
+        {
+            _recipeRepo = recipeRepository;
+            //_tagRepo = tagRepository;
+            _userRepo = userProfileRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Recipe> recipes = _recipeRepo.GetAllRecipes();
+            return Ok(recipes);
+        }
+
+        //[HttpGet("{id}")]
+        //public IActionResult Get(int id)
+        //{
+        //    Recipe recipe = _recipeRepo.GetRecipe(id);
+        //    //List<Tag> tags = _tagRepo.GetTagsByRecipe(id);
+        //    //recipe.Tags = tags;
+        //    return Ok(recipe);
+        //}
+    }
+}
---
+++ GeorgesRecipeRoomFullStack/Repositories/RecipeRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using GeorgesRecipeRoomFullStack.Models;
+using GeorgesRecipeRoomFullStack.Utils;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace GeorgesRecipeRoomFullStack.Repositories
+{
+    public class RecipeRepository : BaseRepository, IRecipeRepository
+    {
+        public RecipeRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Recipe> GetAllRecipes()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT r.Id, r.Title, r.Directions, r.ImageUrl, r.UserProfileId
+                                        FROM Recipe r 
+                                        LEFT JOIN UserProfile u on u.Id = r.UserProfileId";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        List<Recipe> recipes = new List<Recipe>();
+                        while (reader.Read())
+                        {
+                            recipes.Add(new Recipe()
+                            {
+                                Id = DbUtils.GetInt(reader, "Id"),
+                                Title = DbUtils.GetString(reader, "Title"),
+                                Directions = DbUtils.GetString(reader, "Directions"),
+                                ImageUrl = DbUtils.GetString(reader, "ImageUrl"),
+                                Profile = new UserProfile()
+                                {
+                                    Name = DbUtils.GetString(reader, "UserName"),
+                                }
+                            });
+                        }
+
+                        return recipes;
+                   
+                    }
+                }
+            }
+        }
+    }
+}
